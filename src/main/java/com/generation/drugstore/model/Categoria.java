8022a48142cf307da8b59ffc563b3efb9f6f3f18
leosouzv <leogrdss@icloud.com>
2022-05-04 16:31:21 -0300

    Adding Getters and Setters

--- src/main/java/com/generation/drugstore/model/Categoria.java
+++ src/main/java/com/generation/drugstore/model/Categoria.java
@@ -22,4 +22,35 @@ public class Categoria {
 	
 	
 	public double avaliacao;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public double getAvaliacao() {
+		return avaliacao;
+	}
+
+
+	public void setAvaliacao(double avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+	
 }
--- src/main/java/com/generation/drugstore/model/Produto.java
+++ src/main/java/com/generation/drugstore/model/Produto.java
@@ -23,5 +23,29 @@ public class Produto {
 
 	@NotNull
 	public int valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
 	
 }
